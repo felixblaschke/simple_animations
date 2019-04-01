60ca0470bd342540f7980296f7400762b7b49ff1
Felix Blaschke <felix@felix-blaschke.de>
2019-04-01 20:09:03 +0200

    removed invalid dart doc

--- lib/simple_animations.dart
+++ lib/simple_animations.dart
@@ -1,4 +1,3 @@
 library simple_animations;
 
-/// Create awesome animations. Simplified. Well documented. With many examples.
 export 'package:simple_animations/simple_animations/controlled_animation.dart';
