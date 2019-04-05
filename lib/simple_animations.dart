8ee6b93c124e3e1f38975bd5f2a807f2ce1fdf55
Felix Blaschke <felix@felix-blaschke.de>
2019-04-05 15:26:17 +0200

    renamed file to align to dart code guidelines

--- lib/simple_animations.dart
+++ lib/simple_animations.dart
@@ -1,4 +1,4 @@
 library simple_animations;
 
 export 'simple_animations/controlled_animation.dart';
-export 'simple_animations/multi-track-tween.dart';
+export 'simple_animations/multi_track_tween.dart';
