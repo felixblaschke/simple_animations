f11cdc3522b869bd187fa0ec49ddd7f9b1632984
Felix Blaschke <felix@felix-blaschke.de>
2019-05-03 19:55:23 +0200

    added tests and documentation for rendering widget and animation progress

--- lib/simple_animations.dart
+++ lib/simple_animations.dart
@@ -3,4 +3,4 @@ library simple_animations;
 export 'simple_animations/controlled_animation.dart';
 export 'simple_animations/multi_track_tween.dart';
 export 'simple_animations/rendering.dart';
-export 'simple_animations/animation-progress.dart';
+export 'simple_animations/animation_progress.dart';
--- lib/simple_animations/rendering.dart
+++ lib/simple_animations/rendering.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/widgets.dart';
 
+/// Widget to easily create a continuous animation.
+///
+/// You need to specify a [builder] function that gets the build context passed
+/// along with the [timeElapsed] (as a [Duration]) since the rendering started.
+/// You can use this time to specify custom animations on it.
+///
+/// The [builder] rebuilds all sub-widgets every frame.
+///
+/// You define an [onTick] function that is called before builder to update
+/// you rendered scene. It's also utilized during fast-forwarding the animation.
+///
+/// Specify a [startTime] to fast-forward your animation in the beginning.
+/// The widget will interpolate the animation by calling the [onTick] function
+/// multiple times. (Default value is `20`. You can tune it by setting the
+/// [startTimeSimulationTicks] property.)
 class Rendering extends StatefulWidget {
   final Widget Function(BuildContext context, Duration timeElapsed) builder;
   final Function(Duration timeElapsed) onTick;
@@ -47,7 +62,7 @@ class _RenderingState extends State<Rendering>
 
   void _simulateStartTimeTicks() {
     if (widget.onTick != null) {
-      Iterable.generate(widget.startTimeSimulationTicks).forEach((i) {
+      Iterable.generate(widget.startTimeSimulationTicks + 1).forEach((i) {
         final simulatedTime = Duration(
             milliseconds: (widget.startTime.inMilliseconds *
                     i /
