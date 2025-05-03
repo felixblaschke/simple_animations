12cc834095ae257d2d7c59752d5c2d995ab55703
Felix Blaschke <felix@felix-blaschke.de>
2025-05-03 12:42:07 +0200

    Compute red value in test case was updated to due more accurate test runner timings

--- test/movie_tween/multiple_properties_test.dart
+++ test/movie_tween/multiple_properties_test.dart
@@ -7,14 +7,18 @@ void main() {
     var movie = MovieTween();
     movie
         .scene(
-            begin: const Duration(seconds: 0),
-            duration: const Duration(seconds: 1))
+          begin: const Duration(seconds: 0),
+          duration: const Duration(seconds: 1),
+        )
         .tween('width', Tween(begin: 100.0, end: 200.0))
         .tween('height', Tween(begin: 200.0, end: 100.0))
         .tween(
-            'color',
-            ColorTween(
-                begin: const Color(0xFF000000), end: const Color(0xFFFF0000)));
+          'color',
+          ColorTween(
+            begin: const Color(0xFF000000),
+            end: const Color(0xFFFF0000),
+          ),
+        );
 
     expect(movie.duration, const Duration(seconds: 1));
 
@@ -26,35 +30,48 @@ void main() {
     expect(movie.transform(0.5 / 1.0).get<double>('height'), 150.0);
     expect(movie.transform(1.0 / 1.0).get<double>('height'), 100.0);
 
-    expect(movie.transform(0.0 / 1.0).get<Color>('color'),
-        const Color(0xFF000000));
-    expect(movie.transform(0.5 / 1.0).get<Color>('color'),
-        const Color(0xFF7f0000));
-    expect(movie.transform(1.0 / 1.0).get<Color>('color'),
-        const Color(0xFFFF0000));
+    expect(
+      movie.transform(0.0 / 1.0).get<Color>('color'),
+      const Color(0xFF000000),
+    );
+    expect(
+      movie.transform(0.5 / 1.0).get<Color>('color'),
+      const Color.from(alpha: 1.0, red: 0.5, green: 0.0, blue: 0.0),
+    );
+    expect(
+      movie.transform(1.0 / 1.0).get<Color>('color'),
+      const Color(0xFFFF0000),
+    );
   });
 
   test('multi props - cascaded', () {
     var movie = MovieTween();
     movie
         .scene(
-            begin: const Duration(seconds: 0),
-            duration: const Duration(seconds: 2))
+          begin: const Duration(seconds: 0),
+          duration: const Duration(seconds: 2),
+        )
         .tween('width', Tween(begin: 100.0, end: 200.0));
 
     movie
         .scene(
-            begin: const Duration(seconds: 1), end: const Duration(seconds: 3))
+          begin: const Duration(seconds: 1),
+          end: const Duration(seconds: 3),
+        )
         .tween('height', Tween(begin: 200.0, end: 100.0));
 
     movie
         .scene(
-            end: const Duration(seconds: 4),
-            duration: const Duration(seconds: 2))
+          end: const Duration(seconds: 4),
+          duration: const Duration(seconds: 2),
+        )
         .tween(
-            'color',
-            ColorTween(
-                begin: const Color(0xFF000000), end: const Color(0xFFFF0000)));
+          'color',
+          ColorTween(
+            begin: const Color(0xFF000000),
+            end: const Color(0xFFFF0000),
+          ),
+        );
 
     expect(movie.duration, const Duration(seconds: 4));
 
@@ -70,15 +87,25 @@ void main() {
     expect(movie.transform(3.0 / 4.0).get<double>('height'), 100.0);
     expect(movie.transform(4.0 / 4.0).get<double>('height'), 100.0);
 
-    expect(movie.transform(0.0 / 4.0).get<Color>('color'),
-        const Color(0xFF000000));
-    expect(movie.transform(1.0 / 4.0).get<Color>('color'),
-        const Color(0xFF000000));
-    expect(movie.transform(2.0 / 4.0).get<Color>('color'),
-        const Color(0xFF000000));
-    expect(movie.transform(3.0 / 4.0).get<Color>('color'),
-        const Color(0xFF7f0000));
-    expect(movie.transform(4.0 / 4.0).get<Color>('color'),
-        const Color(0xFFFF0000));
+    expect(
+      movie.transform(0.0 / 4.0).get<Color>('color'),
+      const Color(0xFF000000),
+    );
+    expect(
+      movie.transform(1.0 / 4.0).get<Color>('color'),
+      const Color(0xFF000000),
+    );
+    expect(
+      movie.transform(2.0 / 4.0).get<Color>('color'),
+      const Color(0xFF000000),
+    );
+    expect(
+      movie.transform(3.0 / 4.0).get<Color>('color'),
+      const Color.from(alpha: 1.0, red: 0.5, green: 0.0, blue: 0.0),
+    );
+    expect(
+      movie.transform(4.0 / 4.0).get<Color>('color'),
+      const Color(0xFFFF0000),
+    );
   });
 }
--- test/movie_tween/typed_property_test.dart
+++ test/movie_tween/typed_property_test.dart
@@ -11,23 +11,30 @@ void main() {
     var movie = MovieTween();
     movie
         .scene(
-            begin: const Duration(seconds: 0),
-            duration: const Duration(seconds: 2))
+          begin: const Duration(seconds: 0),
+          duration: const Duration(seconds: 2),
+        )
         .tween<double>(_width, Tween(begin: 100.0, end: 200.0));
 
     movie
         .scene(
-            begin: const Duration(seconds: 1), end: const Duration(seconds: 3))
+          begin: const Duration(seconds: 1),
+          end: const Duration(seconds: 3),
+        )
         .tween(_height, Tween(begin: 200.0, end: 100.0));
 
     movie
         .scene(
-            end: const Duration(seconds: 4),
-            duration: const Duration(seconds: 2))
+          end: const Duration(seconds: 4),
+          duration: const Duration(seconds: 2),
+        )
         .tween(
-            _color,
-            ColorTween(
-                begin: const Color(0xFF000000), end: const Color(0xFFFF0000)));
+          _color,
+          ColorTween(
+            begin: const Color(0xFF000000),
+            end: const Color(0xFFFF0000),
+          ),
+        );
 
     expect(movie.duration, const Duration(seconds: 4));
 
@@ -46,7 +53,10 @@ void main() {
     expect(_color.from(movie.transform(0.0 / 4.0)), const Color(0xFF000000));
     expect(_color.from(movie.transform(1.0 / 4.0)), const Color(0xFF000000));
     expect(_color.from(movie.transform(2.0 / 4.0)), const Color(0xFF000000));
-    expect(_color.from(movie.transform(3.0 / 4.0)), const Color(0xFF7f0000));
+    expect(
+      _color.from(movie.transform(3.0 / 4.0)),
+      const Color.from(alpha: 1.0, red: 0.5, green: 0.0, blue: 0.0),
+    );
     expect(_color.from(movie.transform(4.0 / 4.0)), const Color(0xFFFF0000));
   });
 }
